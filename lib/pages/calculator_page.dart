e8023cc7f7cd78c36ccbd515030bcb14e2f433d2
joseluis <joseluisstacruz@gmail.com>
2021-04-17 08:19:50 -0400

    landscape design page calculator

--- lib/pages/calculator_page.dart
+++ lib/pages/calculator_page.dart
@@ -13,52 +13,56 @@ class CalculatorPage extends StatelessWidget {
           title: Text('Calculadora Simple'),
         ),
         backgroundColor: Color.fromRGBO(62, 64, 71, 1.0),
-        body: Column(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(110, 111, 105, 1.0),
-                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+        body: OrientationBuilder(
+          builder: (context, orientation) => Column(
+            children: [
+              Expanded(
+                flex: orientation == Orientation.portrait ? 1 : 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Container(
-                    padding: EdgeInsets.all(20),
-                    alignment: Alignment.centerRight,
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Text(
-                        '5+5-8/log(5)5+5-8/log(5)5+5-8/log(5)5+5-8/9*(5)5+5-8/log(5)5+5-8/log(5)5+5-8/log(5)5+5-8/9*(5)5+5-8/log(5)5+5-8/log(5)5+5-8/log(5)5+5-8/9*(5)5+5-8/log(5)5+5-8/log(5)5+5-8/log(5)5+5-8/9*(5)',
-                        style: TextStyle(fontSize: 40, fontFamily: 'DsDigit'),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(110, 111, 105, 1.0),
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      alignment: Alignment.centerRight,
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Text(
+                          '5+5-8/log(5)5+5-8/log(5)5+5-8/log(5)5+5-8/9*(5)5+5-8/log(5)5+5-8/log(5)5+5-8/log(5)5+5-8/9*(5)5+5-8/log(5)5+5-8/log(5)5+5-8/log(5)5+5-8/9*(5)5+5-8/log(5)5+5-8/log(5)5+5-8/log(5)5+5-8/9*(5)',
+                          style: TextStyle(fontSize: 40, fontFamily: 'DsDigit'),
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                child: SingleChildScrollView(
-                  child: GridView.builder(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: 50,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 4,
-                          crossAxisSpacing: 10,
-                          mainAxisSpacing: 10),
-                      itemBuilder: (context, index) {
-                        return Button(
-                          buttonText: '$index',
-                          color: Color.fromRGBO(126, 112, 112, 1.0),
-                          textColor: Colors.white,
-                        );
-                      }),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  child: SingleChildScrollView(
+                    child: GridView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: 50,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount:
+                                orientation == Orientation.portrait ? 4 : 8,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10),
+                        itemBuilder: (context, index) {
+                          return Button(
+                            buttonText: '$index',
+                            color: Color.fromRGBO(126, 112, 112, 1.0),
+                            textColor: Colors.white,
+                          );
+                        }),
+                  ),
                 ),
               ),
-            )
-          ],
+            ],
+          ),
         ));
   }
 }
